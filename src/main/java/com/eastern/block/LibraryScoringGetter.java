303ca6401cc2a0d2afcd37aca35ca906db65fd90
Andrei Kiryieuski <AndreKiryieuski@sabre.com>
2020-02-20 20:00:54 +0100

    1

---
+++ src/main/java/com/eastern/block/LibraryScoringGetter.java
@@ -0,0 +1,20 @@
+package com.eastern.block;
+
+import com.eastern.block.data.Book;
+import com.eastern.block.data.Library;
+
+import java.util.List;
+import java.util.Set;
+
+public class LibraryScoringGetter {
+    public Integer getScore(Library library, Integer deysLeft, Set<Integer> scanned) {
+        Integer signupProcessTime = library.getSignupProcessTime();
+        List<Book> books = library.getBooks();
+
+        int maxNumberOfBooksPossibleToSend = (deysLeft - signupProcessTime) * library.getShipmentSpeed();
+        int limit = Math.min(maxNumberOfBooksPossibleToSend, library.getNumberOfBooks());
+        int score = books.stream().filter(book -> !scanned.contains(book.getIndex())).limit(limit).mapToInt(Book::getScore).sum();
+
+        return score;
+    }
+}
--- src/main/java/com/eastern/block/algorithm/Algorithm.java
+++ src/main/java/com/eastern/block/algorithm/Algorithm.java
@@ -1,11 +1,38 @@
 package com.eastern.block.algorithm;
 
+import com.eastern.block.LibraryScoringGetter;
 import com.eastern.block.data.InputData;
+import com.eastern.block.data.Library;
 import com.eastern.block.data.OutputData;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Algorithm {
 
+    private LibraryScoringGetter libraryScoringGetter;
+    private Integer daysLeftOnStart;
+
+    public Algorithm(LibraryScoringGetter libraryScoringGetter) {
+        this.libraryScoringGetter = libraryScoringGetter;
+    }
+
     public OutputData calculate(InputData inputData) {
-        return new OutputData();
+        List<Library> libraryList = inputData.getLibraries();
+        Set<Integer> scanned = new HashSet<>();
+        daysLeftOnStart = inputData.getDaysForScannig();
+
+        int daysLeft = daysLeftOnStart;
+        for (Library library : libraryList) {
+            Integer libraryScore = libraryScoringGetter.getScore(library, daysLeft, scanned);
+
+            daysLeft--;
+        }
+
+    }
+
+    private Library getLibraryToScanning(List<Library> ){
+        return
     }
 }
